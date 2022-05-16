55cb4ea0458d5a7cdc33df755dd0f5647e8489f2
centraldogma99 <central.dogma99@gmail.com>
2022-05-17 01:15:30 +0900

    refactor: 로그인 입력 하드코딩 제거

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,131 @@
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+import styled from "@emotion/styled";
+import { palette } from "./palette";
+
+interface NumberInputProps {}
+
+const NumberInput = React.forwardRef<
+  HTMLInputElement,
+  NumberInputProps & React.HTMLProps<HTMLInputElement>
+>(({ ...rest }, ref) => (
+  <input
+    type={"numeric"}
+    inputMode={"decimal"}
+    maxLength={1}
+    placeholder={"-"}
+    ref={ref}
+    {...rest}
+  />
+));
+
+const Input = styled(NumberInput)`
+  border: 2px dashed ${palette.blue};
+  font-size: 1.25rem;
+  padding: 4px;
+  text-align: center;
+  width: 1.75rem;
+  height: 2rem;
+  border-radius: 0.25rem;
+  margin-left: 0.5rem;
+  caret-color: transparent;
+
+  &:first-of-type {
+    margin-left: 0;
+  }
+  &:focus {
+    outline: none;
+    border: ${palette.blue} solid 2.5px;
+  }
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+interface Props {
+  values: string[];
+  setValues: React.Dispatch<React.SetStateAction<string[]>>;
+  length: number;
+  isPasswordCorrect: boolean;
+  onSuccessCallback: () => void;
+  onFailCallback: () => void;
+}
+
+const PasswordInput = ({
+  values,
+  setValues,
+  length,
+  isPasswordCorrect,
+  onSuccessCallback,
+  onFailCallback,
+}: Props) => {
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const setValue = useCallback(
+    (index: number, value: string) => {
+      setValues((prev) => [
+        ...prev.slice(0, index),
+        value,
+        ...prev.slice(index + 1),
+      ]);
+    },
+    [setValues]
+  );
+
+  const handleChange = useCallback(
+    (i: number) => (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      if (!value || isNaN(parseInt(value))) return;
+      setValue(i, value);
+
+      if (i === length - 1) return;
+      inputRefs.current[i + 1]?.focus();
+    },
+    [length, setValue]
+  );
+
+  const isInputFull = useMemo(() => {
+    return values.length === length && values.every((v) => v !== "");
+  }, [length, values]);
+
+  useEffect(() => {
+    if (!isInputFull) return;
+    if (!isPasswordCorrect) {
+      inputRefs.current[0]?.focus();
+      onFailCallback();
+      return setValues(Array(length).fill(""));
+    }
+
+    onSuccessCallback();
+  }, [
+    isInputFull,
+    isPasswordCorrect,
+    length,
+    onFailCallback,
+    onSuccessCallback,
+    setValues,
+  ]);
+
+  return (
+    <InputsContainer>
+      {Array(length)
+        .fill(0)
+        .map((_, i) => (
+          <Input
+            value={values[i]}
+            onChange={handleChange(i)}
+            ref={(e) => (inputRefs.current[i] = e)}
+          />
+        ))}
+    </InputsContainer>
+  );
+};
+
+export default PasswordInput;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,8 @@
-import React, {
-  ChangeEvent,
-  useCallback,
-  useRef,
-  useState,
-  useMemo,
-  useEffect,
-} from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import styled from "@emotion/styled";
 import { palette } from "../components/palette";
 import Spacing from "../components/Spacing";
+import PasswordInput from "../components/PasswordInput";
 
 const Container = styled.div`
   position: fixed;
@@ -36,49 +30,6 @@ const Text = styled.div`
   font-weight: bold;
 `;
 
-interface NumberInputProps {
-  innerRef?: React.RefObject<HTMLInputElement>;
-}
-
-const NumberInput = ({
-  innerRef,
-  ...rest
-}: NumberInputProps & React.HTMLProps<HTMLInputElement>) => (
-  <input
-    type={"numeric"}
-    inputMode={"decimal"}
-    maxLength={1}
-    placeholder={"-"}
-    ref={innerRef}
-    {...rest}
-  />
-);
-
-const Input = styled(NumberInput)`
-  border: 2px dashed ${palette.blue};
-  font-size: 1.25rem;
-  padding: 4px;
-  text-align: center;
-  width: 1.75rem;
-  height: 2rem;
-  border-radius: 0.25rem;
-  margin-left: 0.5rem;
-  caret-color: transparent;
-
-  &:first-of-type {
-    margin-left: 0;
-  }
-  &:focus {
-    outline: none;
-    border: ${palette.blue} solid 2.5px;
-  }
-`;
-
-const InputsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
 const MessageContainer = styled.div<{ isError?: boolean }>`
   margin-top: 32px;
   color: ${({ isError }) => (isError ? "red" : palette.blue)};
@@ -92,87 +43,32 @@ interface Props {
 const LoginPage = ({ onSuccessCallback }: Props) => {
   const [values, setValues] = useState<string[]>(["", "", "", ""]);
   const [isError, setIsError] = useState<boolean>(false);
-  const input1 = useRef<HTMLInputElement>(null);
-  const input2 = useRef<HTMLInputElement>(null);
-  const input3 = useRef<HTMLInputElement>(null);
-  const input4 = useRef<HTMLInputElement>(null);
-
-  const setValue = useCallback((index: number, value: string) => {
-    setValues((prev) => [
-      ...prev.slice(0, index),
-      value,
-      ...prev.slice(index + 1),
-    ]);
-  }, []);
-
-  const handleChange = useCallback(
-    (i: number) => (e: ChangeEvent<HTMLInputElement>) => {
-      const value = e.target.value;
-      if (value === "") return setValue(i, "");
-      if (!value || isNaN(parseInt(value))) return;
-      setValue(i, value);
-      if (isError) setIsError(false);
-      switch (i) {
-        case 0:
-          input2.current?.focus();
-          break;
-        case 1:
-          input3.current?.focus();
-          break;
-        case 2:
-          input4.current?.focus();
-          break;
-      }
-    },
-    [isError, setValue]
-  );
 
   const isPasswordCorrect = useMemo(() => {
     return ["0", "5", "3", "1"].every((v, i) => v === values[i]);
   }, [values]);
 
-  const isInputFull = useMemo(() => {
-    return values.length === 4 && values.every((v) => v !== "");
-  }, [values]);
+  const onFailCallback = useCallback(() => {
+    setIsError(true);
+  }, []);
 
-  useEffect(() => {
-    if (isPasswordCorrect) {
-      setTimeout(onSuccessCallback, 1300);
-      return;
-    } else if (isInputFull) {
-      setValues(["", "", "", ""]);
-      setIsError(true);
-      input1.current?.focus();
-    }
-  }, [onSuccessCallback, values.length, isInputFull, isPasswordCorrect]);
+  const onSuccess = useCallback(() => {
+    setTimeout(onSuccessCallback, 1300);
+  }, [onSuccessCallback]);
 
   return (
     <Container>
       <CenterContainer>
         <Text>비밀번호를 입력하세요!</Text>
         <Spacing size={48} />
-        <InputsContainer>
-          <Input
-            value={values[0]}
-            onChange={handleChange(0)}
-            innerRef={input1}
-          />
-          <Input
-            value={values[1]}
-            onChange={handleChange(1)}
-            innerRef={input2}
-          />
-          <Input
-            value={values[2]}
-            onChange={handleChange(2)}
-            innerRef={input3}
-          />
-          <Input
-            value={values[3]}
-            onChange={handleChange(3)}
-            innerRef={input4}
-          />
-        </InputsContainer>
+        <PasswordInput
+          values={values}
+          setValues={setValues}
+          length={4}
+          isPasswordCorrect={isPasswordCorrect}
+          onSuccessCallback={onSuccess}
+          onFailCallback={onFailCallback}
+        />
         <MessageContainer isError={isError}>
           {isError && "틀렸습니다!"}
           {isPasswordCorrect && "정답입니다! 잠시 후에 이동합니다..."}
