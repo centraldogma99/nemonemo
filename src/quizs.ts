43971fd0012fdc6105a2a1c673bd9c83f48e55ff
junyeong <jychoi@jobis.co>
2022-05-31 21:43:38 +0900

    fix: 퀴즈 툴팁 오류 수정

--- src/quizs.ts
+++ src/quizs.ts
@@ -1,8 +1,8 @@
 const quizs = [
   {
     name: "오리",
-    colSize: 25,
-    rowSize: 25,
+    colSize: 15,
+    rowSize: 15,
     code: "56 124 247 254 124 56 124 16894 29582 32630 16118 16332 8184 1760 504 15",
   },
   {
